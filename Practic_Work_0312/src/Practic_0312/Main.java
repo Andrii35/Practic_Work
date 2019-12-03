66aa20a2def4272c1b9c6718219f09c7e8f7e3d0
Andriy <andriy_nazaruk@yahoo.de>
2019-12-03 12:22:09 +0200

    Practic work first

---
+++ Practic_Work_0312/src/Practic_0312/Main.java
@@ -0,0 +1,26 @@
+package Practic_0312;
+
+import java.lang.Math;// підключення бібліотек
+
+public class Main {
+
+	public static void main(String[] args) {
+		//R=x2(x+1)/b-sin2(x+a);
+		//s=sqrt x*b/a+cos2(x+b)3
+		
+		double a=0.7,b=0.05,x=0.5;
+		double R=0;
+		double s=0;
+	    R=Math.pow(x, 2)*(x+1)/b-Math.pow(Math.sin(x+a),2);
+	    s=Math.sqrt((x*b)/a)+Math.pow(Math.cos(Math.pow(x+b, 3)),2);
+	    
+	    //System.out.printf("R=%5.2f",R);
+	    //System.out.printf("\ns=%5.2f",s);
+	    System.out.printf("R="+Math.round(R));
+	    System.out.printf("\ns="+Math.round(s));
+	    
+
+	}
+
+}
+
---
+++ Practic_Work_0312/src/Practic_0312/Student.java
@@ -0,0 +1,19 @@
+package Practic_0312;
+
+import java.util.Scanner;
+public class Student {
+
+	String name;
+	String surname;
+	int age;
+	String birthday;
+	public Student()
+	{
+		
+	}
+	public Student(String name, String surname, int age, String birthday) {
+		this.name=name;
+		this.surname=surname;
+		
+	}
+}
